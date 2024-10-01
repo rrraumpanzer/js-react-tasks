ff754f1243e5c4b26c9e68b38212660e8e761e8e
rrraumpanzer <ayylmao3713@gmail.com>
2024-10-01 20:58:30 +0300

    3-4

--- 3/src/Card.jsx
+++ 3/src/Card.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Card extends React.Component {
+    render() {
+      return (
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">{this.props.title || 'title'}</h4>
+            <p className="card-text">{this.props.text || 'text'}</p>
+          </div>
+        </div>
+      )
+    }
+  }
 // END
--- 4/src/DefinitionsList.jsx
+++ 4/src/DefinitionsList.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+export default class DefinitionsList extends React.Component {
+    render() {
+        const { data } = this.props;
 
+        if (data.length === 0) {
+            return null;
+        }
+
+        return (
+            <dl>
+            {data.map((item) => (
+                <>
+                    <dt>{item.dt}</dt>
+                    <dd>{item.dd}</dd>
+                </>
+            ))}
+            </dl>
+        );
+    }
+}
 // END
