e293b498d2b4136bf7dd900ea908cf0bdbf13e93
rrraumpanzer <ayylmao3713@gmail.com>
2024-10-03 17:51:06 +0300

    6-7

--- 6/src/Alert.jsx
+++ 6/src/Alert.jsx
@@ -2,5 +2,16 @@ import cn from 'classnames';
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class Alert extends React.Component {
+    render() {
+        const { type, text } = this.props;
+        
+        const AlertClass = cn("alert", `alert-${type}`)
+        return (
+            <div className={AlertClass} role="alert">
+              {text}
+            </div>
+        );
+    }
+}
 // END
--- 7/src/ListGroup.jsx
+++ 7/src/ListGroup.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default class ListGroup extends React.Component {
+    render() {
+        const { children } = this.props;
+        const editedChildren = React.Children.map(children, child => {
+            return (
+                <li className="list-group-item">
+                    {child}
+                </li>
+            );
+        });
+        return (
+            <ul className="list-group">
+                {editedChildren}
+            </ul>
+        );
+    }
+}
 // END
